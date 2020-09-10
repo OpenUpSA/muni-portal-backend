837a31f54a7b340d0666909393e1d7dd208566ef
JD Bothma <jbothma@gmail.com>
2020-09-10 21:38:21 +0200

    Add field for service icon class

--- muni_portal/core/models.py
+++ muni_portal/core/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
@@ -5,20 +6,29 @@ from wagtail.api import APIField
 
 
 class HomePage(Page):
-    subpage_types = ['core.ServicesIndexPage',]
+    subpage_types = [
+        "core.ServicesIndexPage",
+    ]
 
 
 class ServicesIndexPage(Page):
-    subpage_types = ['core.ServicePage',]
+    subpage_types = [
+        "core.ServicePage",
+    ]
 
 
 class ServicePage(Page):
     subpage_types = []
 
+    icon_classes = models.CharField(max_length=250)
     overview = RichTextField()
 
-    content_panels = Page.content_panels + [FieldPanel('overview'),]
+    content_panels = Page.content_panels + [
+        FieldPanel("icon_classes"),
+        FieldPanel("overview"),
+    ]
 
     api_fields = [
-        APIField('overview'),
+        APIField("icon_classes"),
+        APIField("overview"),
     ]
