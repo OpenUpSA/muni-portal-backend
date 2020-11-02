23685c32dd70e905a8fa081a0ff4e32ad440b881
Alexander Chaika <manti.by@gmail.com>
2020-11-02 14:03:40 +0300

    Add test for profile images

--- muni_portal/tests/test_api.py
+++ muni_portal/tests/test_api.py
@@ -1,11 +1,11 @@
-import logging
-
 from django.test import Client, TestCase
 from django.urls import reverse
 from rest_framework import status
-from wagtail.core.models import Site
+from wagtail.core.models import Site, Page
+from wagtail.images.models import Image
+from wagtail.images.tests.utils import get_test_image_file
 
-logger = logging.getLogger(__name__)
+from muni_portal.core.models import ServicePage, AdministratorPage
 
 
 class ApiTestCase(TestCase):
@@ -22,6 +22,38 @@ class ApiTestCase(TestCase):
     def test_pages(self):
         for site in Site.objects.all():
             for page in site.root_page.get_descendants():
-                logger.info(f"Testing page {page.title}")
                 response = self.client.get(self.url + f"{page.id}/")
                 assert response.status_code == status.HTTP_200_OK
+
+
+class ApiImagesTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("wagtailapi:pages:listing")
+
+    def test_service_page_image(self):
+        profile_image = Image.objects.create(
+            file=get_test_image_file()
+        )
+        head_of_service = AdministratorPage.objects.create(
+            title="Test AdministratorPage",
+            slug="test-administrator-page",
+            path="00011111",
+            depth=2,
+            profile_image=profile_image,
+        )
+        service_page = ServicePage(
+            title="Test ServicePage",
+            slug="test-service-page",
+            path="00012222",
+            depth=2,
+            live=True,
+            icon_classes=[],
+            head_of_service=head_of_service
+        )
+        Site.objects.first().root_page.add_child(instance=service_page)
+        response = self.client.get(self.url + f"{service_page.id}/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()["head_of_service"]["profile_image"]
+        assert response.json()["head_of_service"]["profile_image_thumbnail"]
