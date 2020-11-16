a48544f26c0654162dd7e97c23fa4464c6307431
Alexander Chaika <manti.by@gmail.com>
2020-11-16 18:57:18 +0300

    Make web push subscriptions not editable in admin

--- muni_portal/core/admin.py
+++ muni_portal/core/admin.py
@@ -13,3 +13,12 @@ class WebhookAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "endpoint", "created_at")
     list_filter = ("created_at",)
     raw_id_fields = ("user",)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
