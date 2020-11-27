91399bae04db53412a35080d010f3480fc37592a
Alexander Chaika <manti.by@gmail.com>
2020-11-27 14:10:52 +0300

    Update default allowed CORS headers

--- muni_portal/settings.py
+++ muni_portal/settings.py
@@ -14,6 +14,7 @@ import environ
 import re
 
 from datetime import timedelta
+from corsheaders.defaults import default_headers
 
 ROOT_DIR = environ.Path(__file__) - 2
 PROJ_DIR = ROOT_DIR.path("muni_portal")
@@ -201,14 +202,7 @@ FRONTEND_BASE_URL = env.str("FRONTEND_BASE_URL", None)
 
 CORS_URLS_REGEX = r"^/api/(accounts|token)/.*$"
 CORS_ALLOWED_ORIGIN_REGEXES = [re.compile(x) for x in env.list("CORS_ALLOWED_ORIGIN_REGEXES", [])]
-CORS_ALLOW_HEADERS = [
-    "accept",
-    "accept-encoding",
-    "content-type",
-    "charset",
-    "origin",
-    "http_authorization",
-]
+CORS_ALLOW_HEADERS = default_headers
 
 DEFAULT_FILE_STORAGE = env.str("DEFAULT_FILE_STORAGE", 'django.core.files.storage.FileSystemStorage')
 AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID", None)
