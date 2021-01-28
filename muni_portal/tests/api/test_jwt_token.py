613964fd4b0fb7222916f3fde6ad6c48d29a3fe2
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-01-28 16:34:13 +0200

    Add negative test for refresh token

--- muni_portal/tests/api/test_jwt_token.py
+++ muni_portal/tests/api/test_jwt_token.py
@@ -28,10 +28,17 @@ class ApiJWTTokenTestCase(TestCase):
     def test_refresh_token(self):
         login_data = {"username": self.user.username, "password": self.password}
         login_response = self.client.post(reverse("token_obtain_pair"), data=login_data)
-        self.assertEquals(login_response.status_code, status.HTTP_200_OK)
         self.assertIn("refresh", login_response.data)
 
         refresh_data = {"refresh": login_response.data.get("refresh")}
         refresh_response = self.client.post(reverse("token_refresh"), data=refresh_data)
         self.assertEquals(refresh_response.status_code, status.HTTP_200_OK)
         self.assertIn("access", refresh_response.data)
+
+    def test_invalid_refresh_token_fail(self):
+        login_data = {"username": self.user.username, "password": self.password}
+        self.client.post(reverse("token_obtain_pair"), data=login_data)
+
+        refresh_data = {"refresh": "invalid refresh token"}
+        refresh_response = self.client.post(reverse("token_refresh"), data=refresh_data)
+        self.assertEquals(refresh_response.status_code, status.HTTP_401_UNAUTHORIZED)
