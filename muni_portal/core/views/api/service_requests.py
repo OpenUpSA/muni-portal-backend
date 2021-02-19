a874f34c5c37927e92351ec5d0ea17d64c03a03d
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-02-19 09:55:04 +0200

    Add ServiceRequestSerializer

--- muni_portal/core/views/api/service_requests.py
+++ muni_portal/core/views/api/service_requests.py
@@ -1,5 +1,25 @@
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse, Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from muni_portal.core.models import ServiceRequest
 
 
+@require_http_methods(["GET"])
 def service_request_detail(request) -> None:
     """ Service Request detail view """
-    pass
+
+
+class ServiceRequestApiView(APIView):
+
+    def get_object(self, pk: int):
+        try:
+            ServiceRequest.objects.get(pk=pk)
+        except ServiceRequest.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        service_request = self.get_object(pk)
+        serializer = ServiceRequestSerializer(service_request)
+        return Response(serializer.data)
