b236367514402e5a87c77d00553aea1482f43e0e
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-02-19 10:27:27 +0200

    Add ServiceRequestObject type

--- muni_portal/collaborator_api/client.py
+++ muni_portal/collaborator_api/client.py
@@ -40,6 +40,27 @@ class FormField(TypedDict):
     FieldValue: str
 
 
+class ServiceRequestObject(TypedDict):
+    """ Service Request object as returned by Collaborator Web API """
+    obj_id: int
+    template_id: int
+    F0: str
+    F1: str
+    F2: str
+    F3: str
+    F4: str
+    F5: str
+    F7: str
+    F8: str
+    F9: str
+    F10: str
+    F11: str
+    F12: str
+    F15: str
+    F19: str
+    F25: str
+
+
 class Client:
     """ Main Collaborator Web API class.
 
@@ -115,7 +136,7 @@ class Client:
         response.raise_for_status()
         return response
 
-    def get_task(self, obj_id: int, template_id: int = 9, fields: List[FormField] = None) -> Dict:
+    def get_task(self, obj_id: int, template_id: int = 9, fields: List[FormField] = None) -> ServiceRequestObject:
         """ Retrieve detail about a task object. """
         self.__assert_auth__()
 
