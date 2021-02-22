992a1b6e3ad804351f5650d8f22bddd55cff7c8b
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-02-22 16:02:03 +0200

    Finish get() view

--- muni_portal/core/views/api/service_requests.py
+++ muni_portal/core/views/api/service_requests.py
@@ -9,22 +9,39 @@ from django.conf import settings
 
 class ServiceRequestApiView(APIView):
 
-    def get_object(self, pk: int):
+    # TODO: perms? depends what the default is
+
+    @staticmethod
+    def get_object(pk: int):
         try:
             return ServiceRequest.objects.get(pk=pk)
         except ServiceRequest.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
+        """
+        Return detail of ServiceRequest object.
+
+        First fetches from Collaborator Web API, then updates local instance with remote instance (if found), then
+        returns local instance.
+        """
         local_object = self.get_object(pk)
         object_id = local_object.collaborator_object_id
+        serializer = ServiceRequestSerializer(local_object)
 
-        print(settings.COLLABORATOR_API_USERNAME, settings.COLLABORATOR_API_PASSWORD)
         client = Client(settings.COLLABORATOR_API_USERNAME, settings.COLLABORATOR_API_PASSWORD)
         client.authenticate()
         remote_object = client.get_task(object_id)
 
-        # serializer = ServiceRequestSerializer(local_object, data=remote_object)
+        serializer.update(local_object, remote_object)
+        return Response(serializer.data)
 
-        # return Response(serializer.data)
-        return Response(remote_object)
+    def list(self, request):
+        """
+        Return list of ServiceRequest objects.
+
+        We build the list by retrieving all local ServiceRequest objects for this user and requesting a detail view
+        of each object from Collaborator Web API and returning it as a list.
+        """
+
+        local_objects = ServiceRequest.objects.filter(user=request.user)
