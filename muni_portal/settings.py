7e2a03a9ddc589c8bee8b22e3ea00128eceea0bc
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-02-22 12:55:43 +0200

    Initialise collaborator settings from env vars

--- muni_portal/settings.py
+++ muni_portal/settings.py
@@ -224,7 +224,14 @@ EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", "apikey")
 EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", None)
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
 
-WAGTAILAPI_LIMIT_MAX=500
+WAGTAILAPI_LIMIT_MAX = 500
+
+# Collaborator Web API
+# https://github.com/OpenUpSA/muni-portal-backend/pull/68
+# https://openup.gitbook.io/cape-agulhas-app/collaborator-integration
+COLLABORATOR_API_USERNAME = env.str("COLLABORATOR_API_USERNAME", None)
+COLLABORATOR_API_PASSWORD = env.str("COLLABORATOR_API_PASSWORD", None)
+COLLABORATOR_API_BASE_URL = env.str("COLLABORATOR_API_BASE_URL", "https://consumercollab.collaboratoronline.com")
 
 # https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
