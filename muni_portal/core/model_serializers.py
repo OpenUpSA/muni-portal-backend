5a9cd93cccb5f8a4bc9775bc05676f0da28474f8
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-02-22 13:50:03 +0200

    Update update() method on serializer

--- muni_portal/core/model_serializers.py
+++ muni_portal/core/model_serializers.py
@@ -7,66 +7,31 @@ class ServiceRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ServiceRequest
-        fields = ['__all__']
+        fields = '__all__'
 
     def update(self, instance, validated_data: ServiceRequestObject):
 
-        # TODO: remove unused fields
-        obj_id = validated_data.get("obj_id", None)
-        template_id = validated_data.get("template_id", None)
-        f0 = validated_data.get("F0", None)
-        f1 = validated_data.get("F1", None)
-        f2 = validated_data.get("F2", None)
-        f3 = validated_data.get("F3", None)
-        f4 = validated_data.get("F4", None)
-        f5 = validated_data.get("F5", None)
-        f6 = validated_data.get("F6", None)
-        f7 = validated_data.get("F7", None)
-        f8 = validated_data.get("F8", None)
-        f9 = validated_data.get("F9", None)
-        f10 = validated_data.get("F10", None)
-        f11 = validated_data.get("F11", None)
-        f12 = validated_data.get("F12", None)
-        f13 = validated_data.get("F13", None)
-        f14 = validated_data.get("F14", None)
-        f15 = validated_data.get("F15", None)
-        f18 = validated_data.get("F18", None)
-        f19 = validated_data.get("F19", None)
-        f25 = validated_data.get("F25", None)
-
-        if f1:
-            instance.type = f1
-        if f2:
-            instance.user_name = f2
-        if f3:
-            instance.user_surname = f3
-        if f4:
-            instance.user_mobile_number = f4
-        if f5:
-            instance.user_email_address = f5
-        if f6:
-            instance.municipal_account_number = f6
-        if f7:
-            instance.street_name = f7
-        if f8:
-            instance_street_number = f8
-        if f9:
-            instance.suburb = f9
-        if f10:
-            instance.description = f10
-        if f11:
-            instance.coordinates = f11
-        if f12:
-            # TODO: convert to date object before saving to instance
-            instance.request_date = f12
-        if f13:
-            instance.mobile_reference = f13
-        if f14:
-            instance.on_premis_reference = f14
-        if f15:
-            instance.status = f15
-        if f25:
-            instance.demarcation_code = f25
+        # obj_id = validated_data.get("obj_id", None)
+        # template_id = validated_data.get("template_id", None)
+        # f0 = validated_data.get("F0", None)
+        instance.type = validated_data.get("F1", instance.type)
+        instance.user_name = validated_data.get("F2", instance.user_name)
+        instance.user_surname = validated_data.get("F3", instance.user_surname)
+        instance.user_mobile_number = validated_data.get("F4", instance.user_mobile_number)
+        instance.user_email_address = validated_data.get("F5", instance.user_email_address)
+        instance.municipal_account_number = validated_data.get("F6", instance.municipal_account_number)
+        instance.street_name = validated_data.get("F7", instance.street_name)
+        instance.street_number = validated_data.get("F8", instance.street_number)
+        instance.suburb = validated_data.get("F9", instance.suburb)
+        instance.description = validated_data.get("F10", instance.description)
+        instance.coordinates = validated_data.get("F11", instance.coordinates)
+        instance.request_date = validated_data.get("F12", instance.request_date)
+        instance.mobile_reference = validated_data.get("F13", instance.mobile_reference)
+        instance.on_premis_reference = validated_data.get("F14", instance.on_premis_reference)
+        instance.status = validated_data.get("F15", instance.status)
+        # f18 = validated_data.get("F18", None)
+        # f19 = validated_data.get("F19", None)
+        instance.demarcation_code = validated_data.get("F25", instance.demarcation_code)
 
         instance.save()
 
--- muni_portal/core/views/api/service_requests.py
+++ muni_portal/core/views/api/service_requests.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from muni_portal.collaborator_api.client import Client
 from muni_portal.core.models import ServiceRequest
-from muni_portal.core.serializers import ServiceRequestSerializer
+from muni_portal.core.model_serializers import ServiceRequestSerializer
 from django.conf import settings
 
 
@@ -11,19 +11,20 @@ class ServiceRequestApiView(APIView):
 
     def get_object(self, pk: int):
         try:
-            ServiceRequest.objects.get(pk=pk)
+            return ServiceRequest.objects.get(pk=pk)
         except ServiceRequest.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        local_service_request = self.get_object(pk)
-        object_id = local_service_request.object_id
+        local_object = self.get_object(pk)
+        object_id = local_object.collaborator_object_id
 
+        print(settings.COLLABORATOR_API_USERNAME, settings.COLLABORATOR_API_PASSWORD)
         client = Client(settings.COLLABORATOR_API_USERNAME, settings.COLLABORATOR_API_PASSWORD)
         client.authenticate()
-        remote_service_request = client.get_task(object_id)
+        remote_object = client.get_task(object_id)
 
-        # TODO: Update local object with fetched object (probably using serializer)
+        # serializer = ServiceRequestSerializer(local_object, data=remote_object)
 
-        serializer = ServiceRequestSerializer(local_service_request)
-        return Response(serializer.data)
+        # return Response(serializer.data)
+        return Response(remote_object)
