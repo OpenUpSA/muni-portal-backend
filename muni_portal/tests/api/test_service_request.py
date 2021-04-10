d276b44084879b0422b57ad457608dc3b9ef557b
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-04-10 18:52:51 +0200

    Add docstrings for each test and format

--- muni_portal/tests/api/test_service_request.py
+++ muni_portal/tests/api/test_service_request.py
@@ -16,11 +16,7 @@ MOCK_GET_TASK_DETAIL_RESPONSE_JSON = {
     "DetailedMessages": None,
     "CollaboratorUri": "https://consumercollab.collaboratoronline.com/collab/",
     "Data": {
-        "tmpCollabObject": {
-            "obj_id": 1,
-            "template_id": 9,
-            "Fields": []
-        },
+        "tmpCollabObject": {"obj_id": 1, "template_id": 9, "Fields": []},
         "ObjectList": [
             [
                 {
@@ -42,11 +38,11 @@ MOCK_GET_TASK_DETAIL_RESPONSE_JSON = {
                     "F15": "Assigned",
                     "F18": "Mr Bothma. Cape Agulhas Municipality confirms receipt of your request. Ref No 373761",
                     "F19": "556704",
-                    "F20": "WC033"
+                    "F20": "WC033",
                 }
             ]
-        ]
-    }
+        ],
+    },
 }
 
 MOCK_GET_TASK_LIST_RESPONSE_JSON = {
@@ -55,11 +51,7 @@ MOCK_GET_TASK_LIST_RESPONSE_JSON = {
     "DetailedMessages": None,
     "CollaboratorUri": "https://consumercollab.collaboratoronline.com/collab/",
     "Data": {
-        "tmpCollabObject": {
-            "obj_id": 1,
-            "template_id": 9,
-            "Fields": []
-        },
+        "tmpCollabObject": {"obj_id": 1, "template_id": 9, "Fields": []},
         "ObjectList": [
             [
                 {
@@ -81,7 +73,7 @@ MOCK_GET_TASK_LIST_RESPONSE_JSON = {
                     "F15": "Assigned",
                     "F18": "Mr Bothma. Cape Agulhas Municipality confirms receipt of your request. Ref No 373761",
                     "F19": "556704",
-                    "F20": "WC033"
+                    "F20": "WC033",
                 },
                 {
                     "obj_id": 2,
@@ -102,24 +94,27 @@ MOCK_GET_TASK_LIST_RESPONSE_JSON = {
                     "F15": "Assigned",
                     "F18": "Mr Bothma. Cape Agulhas Municipality confirms receipt of your request. Ref No 373761",
                     "F19": "556704",
-                    "F20": "WC033"
-                }
+                    "F20": "WC033",
+                },
             ]
-        ]
-    }
+        ],
+    },
 }
 
 
 class ApiServiceRequestTestCase(APITestCase):
-
     @classmethod
     def setUpTestData(cls) -> None:
         cls.password = Faker().password(length=8)
         cls.user = User.objects.create_user(
             username="test", email="test@test.com", password=cls.password
         )
-        cls.service_request_one = ServiceRequest.objects.create(collaborator_object_id=1, user=cls.user)
-        cls.service_request_two = ServiceRequest.objects.create(collaborator_object_id=2, user=cls.user)
+        cls.service_request_one = ServiceRequest.objects.create(
+            collaborator_object_id=1, user=cls.user
+        )
+        cls.service_request_two = ServiceRequest.objects.create(
+            collaborator_object_id=2, user=cls.user
+        )
 
     def authenticate(self) -> None:
         data = {"username": self.user.username, "password": self.password}
@@ -142,13 +137,18 @@ class ApiServiceRequestTestCase(APITestCase):
         return mock_detail_response
 
     @mock.patch("muni_portal.collaborator_api.client.requests.post")
-    @mock.patch.object(Session, 'post')
+    @mock.patch.object(Session, "post")
     def test_get_detail(self, mock_session_post, mock_post):
+        """ Test GET detail view returns correct schema and values """
         mock_post.return_value = self.get_mock_auth_response()
         mock_session_post.return_value = self.get_mock_detail_response()
         self.authenticate()
 
-        response = self.client.get(reverse("service-request-detail", kwargs={"pk": self.service_request_one.pk}))
+        response = self.client.get(
+            reverse(
+                "service-request-detail", kwargs={"pk": self.service_request_one.pk}
+            )
+        )
         self.assertEquals(response.status_code, status.HTTP_200_OK)
 
         expected_response_data = {
@@ -170,13 +170,14 @@ class ApiServiceRequestTestCase(APITestCase):
             "collaborator_status": "assigned",
             "status": "in_progress",
             "demarcation_code": "WC033",
-            "user": 4
+            "user": 4,
         }
         self.assertDictEqual(response.data, expected_response_data)
 
     @mock.patch("muni_portal.collaborator_api.client.requests.post")
-    @mock.patch.object(Session, 'post')
+    @mock.patch.object(Session, "post")
     def test_get_list(self, mock_session_post, mock_post):
+        """ Test GET list returns correct schema and list """
         mock_post.return_value = self.get_mock_auth_response()
         mock_session_post.return_value = self.get_mock_detail_response()
 
@@ -185,53 +186,57 @@ class ApiServiceRequestTestCase(APITestCase):
         response = self.client.get(reverse("service-request-list-create"))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
 
-        expected_response_data = ({
-            "id": 1,
-            "collaborator_object_id": 1,
-            "type": "Sewerage",
-            "user_name": "Mr JD",
-            "user_surname": "Bothma",
-            "user_mobile_number": "0792816737",
-            "user_email_address": "jd@openup.org.za",
-            "municipal_account_number": None,
-            "street_name": "Geen Straat",
-            "street_number": "123",
-            "suburb": "Daar",
-            "description": "This one has the date 2020-02-01 sent from the app",
-            "coordinates": "12.3, 45.6",
-            "request_date": "2020-02-01T00:00:00Z",
-            "on_premis_reference": "373761",
-            "collaborator_status": "assigned",
-            "status": "in_progress",
-            "demarcation_code": "WC033",
-            "user": 4
-        }, {
-            "id": 2,
-            "collaborator_object_id": 2,
-            "type": "Sewerage",
-            "user_name": "Mr JD",
-            "user_surname": "Bothma",
-            "user_mobile_number": "0792816737",
-            "user_email_address": "jd@openup.org.za",
-            "municipal_account_number": None,
-            "street_name": "Geen Straat",
-            "street_number": "123",
-            "suburb": "Daar",
-            "description": "This one has the date 2020-02-01 sent from the app",
-            "coordinates": "12.3, 45.6",
-            "request_date": "2020-02-01T00:00:00Z",
-            "on_premis_reference": "373761",
-            "collaborator_status": "assigned",
-            "status": "in_progress",
-            "demarcation_code": "WC033",
-            "user": 4
-        })
-        sorted_response = sorted(response.data, key=lambda k: k['id'])
+        expected_response_data = (
+            {
+                "id": 1,
+                "collaborator_object_id": 1,
+                "type": "Sewerage",
+                "user_name": "Mr JD",
+                "user_surname": "Bothma",
+                "user_mobile_number": "0792816737",
+                "user_email_address": "jd@openup.org.za",
+                "municipal_account_number": None,
+                "street_name": "Geen Straat",
+                "street_number": "123",
+                "suburb": "Daar",
+                "description": "This one has the date 2020-02-01 sent from the app",
+                "coordinates": "12.3, 45.6",
+                "request_date": "2020-02-01T00:00:00Z",
+                "on_premis_reference": "373761",
+                "collaborator_status": "assigned",
+                "status": "in_progress",
+                "demarcation_code": "WC033",
+                "user": 4,
+            },
+            {
+                "id": 2,
+                "collaborator_object_id": 2,
+                "type": "Sewerage",
+                "user_name": "Mr JD",
+                "user_surname": "Bothma",
+                "user_mobile_number": "0792816737",
+                "user_email_address": "jd@openup.org.za",
+                "municipal_account_number": None,
+                "street_name": "Geen Straat",
+                "street_number": "123",
+                "suburb": "Daar",
+                "description": "This one has the date 2020-02-01 sent from the app",
+                "coordinates": "12.3, 45.6",
+                "request_date": "2020-02-01T00:00:00Z",
+                "on_premis_reference": "373761",
+                "collaborator_status": "assigned",
+                "status": "in_progress",
+                "demarcation_code": "WC033",
+                "user": 4,
+            },
+        )
+        sorted_response = sorted(response.data, key=lambda k: k["id"])
         self.assertDictEqual(sorted_response[0], expected_response_data[0])
         self.assertDictEqual(sorted_response[1], expected_response_data[1])
 
     @mock.patch("muni_portal.collaborator_api.client.requests.post")
     def test_post_create(self, mock_post):
+        """ Test POST create returns 201 with correct schema and values """
         mock_post.return_value = self.get_mock_auth_response()
         self.authenticate()
 
@@ -243,12 +248,14 @@ class ApiServiceRequestTestCase(APITestCase):
             "street_name": "test street",
             "street_number": "test number",
             "suburb": "JD's burb :)",
-            "description": "test description"
+            "description": "test description",
         }
         response = self.client.post(reverse("service-request-list-create"), data=data)
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
     def test_post_create_local_object(self):
+        """ Test POST create, remove object ID and ensure API returns the same object fully populated without
+        contacting collaborator """
         self.authenticate()
 
         service_type = "test-type"
@@ -268,43 +275,52 @@ class ApiServiceRequestTestCase(APITestCase):
             "street_name": street_name,
             "street_number": street_number,
             "suburb": suburb,
-            "description": description
+            "description": description,
         }
 
         ServiceRequest.objects.all().delete()
 
-        create_response = self.client.post(reverse("service-request-list-create"), data=data)
+        create_response = self.client.post(
+            reverse("service-request-list-create"), data=data
+        )
         self.assertEquals(create_response.status_code, status.HTTP_201_CREATED)
 
         local_object = ServiceRequest.objects.first()
         local_object.collaborator_object_id = None
         local_object.save()
 
-        get_detail_response = self.client.get(reverse("service-request-detail", kwargs={'pk': local_object.pk}))
+        get_detail_response = self.client.get(
+            reverse("service-request-detail", kwargs={"pk": local_object.pk})
+        )
         self.assertEquals(get_detail_response.status_code, status.HTTP_200_OK)
-        self.assertEquals(get_detail_response.data['type'], service_type)
-        self.assertEquals(get_detail_response.data['user_name'], user_name)
-        self.assertEquals(get_detail_response.data['user_surname'], user_surname)
-        self.assertEquals(get_detail_response.data['user_mobile_number'], user_mobile_number)
-        self.assertEquals(get_detail_response.data['street_name'], street_name)
-        self.assertEquals(get_detail_response.data['street_number'], street_number)
-        self.assertEquals(get_detail_response.data['suburb'], suburb)
-        self.assertEquals(get_detail_response.data['description'], description)
+        self.assertEquals(get_detail_response.data["type"], service_type)
+        self.assertEquals(get_detail_response.data["user_name"], user_name)
+        self.assertEquals(get_detail_response.data["user_surname"], user_surname)
+        self.assertEquals(
+            get_detail_response.data["user_mobile_number"], user_mobile_number
+        )
+        self.assertEquals(get_detail_response.data["street_name"], street_name)
+        self.assertEquals(get_detail_response.data["street_number"], street_number)
+        self.assertEquals(get_detail_response.data["suburb"], suburb)
+        self.assertEquals(get_detail_response.data["description"], description)
 
         get_list_response = self.client.get(reverse("service-request-list-create"))
         self.assertEquals(get_list_response.status_code, status.HTTP_200_OK)
-        self.assertEquals(get_list_response.data[0]['type'], service_type)
-        self.assertEquals(get_list_response.data[0]['user_name'], user_name)
-        self.assertEquals(get_list_response.data[0]['user_surname'], user_surname)
-        self.assertEquals(get_list_response.data[0]['user_mobile_number'], user_mobile_number)
-        self.assertEquals(get_list_response.data[0]['street_name'], street_name)
-        self.assertEquals(get_list_response.data[0]['street_number'], street_number)
-        self.assertEquals(get_list_response.data[0]['suburb'], suburb)
-        self.assertEquals(get_list_response.data[0]['description'], description)
+        self.assertEquals(get_list_response.data[0]["type"], service_type)
+        self.assertEquals(get_list_response.data[0]["user_name"], user_name)
+        self.assertEquals(get_list_response.data[0]["user_surname"], user_surname)
+        self.assertEquals(
+            get_list_response.data[0]["user_mobile_number"], user_mobile_number
+        )
+        self.assertEquals(get_list_response.data[0]["street_name"], street_name)
+        self.assertEquals(get_list_response.data[0]["street_number"], street_number)
+        self.assertEquals(get_list_response.data[0]["suburb"], suburb)
+        self.assertEquals(get_list_response.data[0]["description"], description)
 
     @mock.patch("muni_portal.collaborator_api.client.requests.post")
-    @mock.patch.object(Session, 'post')
+    @mock.patch.object(Session, "post")
     def test_post_create_validation(self, mock_session_post, mock_post):
+        """ Test that the POST create view returns basic field-level validation errors """
         mock_post.return_value = self.get_mock_auth_response()
         mock_session_post.return_value = self.get_mock_detail_response()
         self.authenticate()
@@ -318,20 +334,26 @@ class ApiServiceRequestTestCase(APITestCase):
             "street_name": "test street",
             "street_number": "test number",
             "suburb": "JD's burb :)",
-            "description": "super long! " * 1000  # make a super long description
+            "description": "super long! " * 1000,  # make a super long description
         }
         response = self.client.post(reverse("service-request-list-create"), data=data)
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        self.assertEquals(type(response.data['description'][0]), ErrorDetail)
-        self.assertEquals(response.data['description'][0], "Ensure this field has no more than 1024 characters.")
+        self.assertEquals(type(response.data["description"][0]), ErrorDetail)
+        self.assertEquals(
+            response.data["description"][0],
+            "Ensure this field has no more than 1024 characters.",
+        )
 
-        self.assertEquals(type(response.data['user_email_address'][0]), ErrorDetail)
-        self.assertEquals(response.data['user_email_address'][0], "Enter a valid email address.")
+        self.assertEquals(type(response.data["user_email_address"][0]), ErrorDetail)
+        self.assertEquals(
+            response.data["user_email_address"][0], "Enter a valid email address."
+        )
 
     @mock.patch("muni_portal.collaborator_api.client.requests.post")
-    @mock.patch.object(Session, 'post')
+    @mock.patch.object(Session, "post")
     def test_post_create_missing_fields(self, mock_session_post, mock_post):
+        """ Test that POST create complains if fields are missing """
         mock_post.return_value = self.get_mock_auth_response()
         mock_session_post.return_value = self.get_mock_detail_response()
         self.authenticate()
@@ -339,11 +361,13 @@ class ApiServiceRequestTestCase(APITestCase):
         response = self.client.post(reverse("service-request-list-create"))
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        self.assertEquals(response.data['type'], 'This field is required.')
-        self.assertEquals(response.data['user_name'], 'This field is required.')
-        self.assertEquals(response.data['user_surname'], 'This field is required.')
-        self.assertEquals(response.data['user_mobile_number'], 'This field is required.')
-        self.assertEquals(response.data['street_name'], 'This field is required.')
-        self.assertEquals(response.data['street_number'], 'This field is required.')
-        self.assertEquals(response.data['suburb'], 'This field is required.')
-        self.assertEquals(response.data['description'], 'This field is required.')
+        self.assertEquals(response.data["type"], "This field is required.")
+        self.assertEquals(response.data["user_name"], "This field is required.")
+        self.assertEquals(response.data["user_surname"], "This field is required.")
+        self.assertEquals(
+            response.data["user_mobile_number"], "This field is required."
+        )
+        self.assertEquals(response.data["street_name"], "This field is required.")
+        self.assertEquals(response.data["street_number"], "This field is required.")
+        self.assertEquals(response.data["suburb"], "This field is required.")
+        self.assertEquals(response.data["description"], "This field is required.")
