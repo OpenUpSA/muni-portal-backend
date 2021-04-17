0bdf6ee91e633f2f503b3f2ecbccbc96360199c0
Jaron Rademeyer <jaronrademeyer@gmail.com>
2021-04-17 13:42:03 +0200

    Add ServiceRequestImage admin page

--- muni_portal/core/admin.py
+++ muni_portal/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from muni_portal.core.models import Webhook, ServiceRequest
+from muni_portal.core.models import Webhook, ServiceRequest, ServiceRequestImage
 
 
 @admin.register(Webhook)
@@ -12,3 +12,9 @@ class WebhookAdmin(admin.ModelAdmin):
 class ServiceRequestAdmin(admin.ModelAdmin):
     list_display = ("id", "collaborator_object_id", "user_name", "user_surname", "user_email_address", "description", "request_date", "status")
     list_filter = ("request_date", "type", "status")
+
+
+@admin.register(ServiceRequestImage)
+class ServiceRequestImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "service_request", "file", "date_created", "exists_on_collaborator")
+    list_filter = ("exists_on_collaborator", "date_created")
