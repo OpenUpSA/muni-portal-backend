00f886acc213eab3b7ce006488a7bec4a19068be
Di M <meerkdir@gmail.com>
2022-08-31 09:14:43 +0200

    Changed F37 True/False to Yes/No

--- muni_portal/core/views/api/service_requests.py
+++ muni_portal/core/views/api/service_requests.py
@@ -172,7 +172,7 @@ class ServiceRequestListCreateView(ServiceRequestAPIView):
             {"FieldID": "F11", "FieldValue": coordinates},
             {"FieldID": "F12", "FieldValue": request_date_iso},
             {"FieldID": "F20", "FieldValue": demarcation_code},
-            {"FieldID": "F37", "FieldValue": True},
+            {"FieldID": "F37", "FieldValue": 'Yes'},
         ]
 
         service_request = ServiceRequest.objects.create(
@@ -261,8 +261,8 @@ class ServiceRequestAttachmentListCreateView(views.APIView):
         if attachments_can_be_created:
             chain.append(update_service_request_record, service_request.id, "Initial")
             chain.append(update_service_request_record, service_request.id, "Registered")
-            chain.append(update_service_request_record, service_request.id, False, 'F37')
-            chain.append(update_service_request_record, service_request.id, True, 'F37')
+            chain.append(update_service_request_record, service_request.id, "No", "F37")
+            chain.append(update_service_request_record, service_request.id, "Yes", "F37")
             chain.run()
 
         return Response(status=201)
